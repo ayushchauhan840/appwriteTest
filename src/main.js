e093df63ecdcb81e77ee9c2f062a3b6a82a253e2
ayush <ashuchauhan840@gmail.com>
2024-07-03 16:28:07 +0530

    added env variable and testing

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-
-
-
 export default async ({ req, res, log, error }) => {
-
-
-
-
-    return res.send('Hello, new World!');
-
-
-  
-
+  if (req.method === "GET") {
+    return res.send('res from get request')
+  }
+  if (req.method === "POST") {
+    return res.json({
+      'sendData': req.body,
+      'NEWENV': process.env.NEW_NAME,
+      'OLDENV': process.env.OLD_NAME,
+      "TESTENV": process.env.TEST_NAME
+    });
+  }
 };
